8c8548a36cbed04da16fbb3bb0aaca5ba78e8bca
Victor Reyes <victor.reyes1@ujcv.edu.hn>
2020-03-19 22:50:22 -0600

    Completando el HeapSort

--- src/edu/ujcv/progra1/HeapSort.java
+++ src/edu/ujcv/progra1/HeapSort.java
@@ -20,13 +20,32 @@ public class HeapSort implements SortTester {
             heapify(array, n, i);
 
 
-        for (int i=n-1; i>=0; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             int temp = array[0];
             array[0] = array[i];
             array[i] = temp;
 
             heapify(array, i, 0);
-            }
         }
     }
-}
+        void heapify(int array[], int n, int i)
+        {
+            int largest = i;
+            int l = 2*i + 1;
+            int r = 2*i + 2;
+
+            if (l < n && array[l] > array[largest])
+                largest = l;
+
+            if (r < n && array[r] > array[largest])
+                largest = r;
+
+            if (largest != i)
+            {
+                int swap = array[i];
+                array[i] = array[largest];
+                array[largest] = swap;
+
+                heapify(array, n, largest);
+            }
+        }
