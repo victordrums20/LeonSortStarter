c58b87c890b4c2ac381a4e4ac823a122a6456aa5
Victor Reyes <victor.reyes1@ujcv.edu.hn>
2020-03-19 22:24:27 -0600

    Agregando al método mergesort

--- src/edu/ujcv/progra1/MergeSort.java
+++ src/edu/ujcv/progra1/MergeSort.java
@@ -30,6 +30,28 @@ public class MergeSort  implements SortTester{
         return merge(mergeSort(parteInferior),mergeSort(parteSuperior));
     }
 
-    public static int[] merge(int[] inf, int[] sup ){
-        
+    public static int[] merge(int[] a, int[] b ) {
+        int[] c = new int[a.length + b.length];
+        int j = 0, k = 0, i;
+        for (i = 0; i < c.length && j < a.length && k < b.length; i++) {
+            if (a[j] < b[k]) {
+                c[i] = a[j++];
+            } else {
+                c[i] = b[k++];
+            }
+
+        }
+
+        for (; i < c.length; i++) {
+            if (j < a.length) {
+                c[i] = a[j++];
+            }
+            if (k < b.length) {
+                if (k < b.length) {
+                    c[i] = b[k++];
+                }
+            }
+        }
+        return c;
+    }
 }
