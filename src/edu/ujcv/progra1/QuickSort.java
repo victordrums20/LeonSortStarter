098b36f8f69f3fd4e21353dad09cb12dd1ca3fc2
Victor Reyes <victor.reyes1@ujcv.edu.hn>
2020-03-19 23:33:47 -0600

    Completando el método QuickSort

--- src/edu/ujcv/progra1/QuickSort.java
+++ src/edu/ujcv/progra1/QuickSort.java
@@ -23,4 +23,22 @@ public class QuickSort implements SortTester {
             quickSort(arr, partition + 1, end);
         }
     }
+
+    public static int partition(int[] arr, int start, int end){
+        int pivot = arr[end];
+
+        for(int i=start; i<end; i++){
+            if(arr[i]<pivot){
+                int temp= arr[start];
+                arr[start]=arr[i];
+                arr[i]=temp;
+                start++;
+            }
+        }
+        int temp = arr[start];
+        arr[start] = pivot;
+        arr[end] = temp;
+
+        return start;
+    }
 }
