777eb819ae3069ffa283a342af832d0b439438f1
Victor Reyes <victor.reyes1@ujcv.edu.hn>
2020-03-19 22:58:27 -0600

    Finalizando el HeapSort

--- src/edu/ujcv/progra1/HeapSort.java
+++ src/edu/ujcv/progra1/HeapSort.java
@@ -1,7 +1,5 @@
 package edu.ujcv.progra1;
 
-import java.util.PriorityQueue;
-
 public class HeapSort implements SortTester {
     @Override
     public long sort(int[] array) {
@@ -13,7 +11,7 @@ public class HeapSort implements SortTester {
         return end - start;
     }
 
-    public static int[] heapSort(int[] array) {
+    public static void heapSort(int[] array) {
         int n = array.length;
 
         for (int i = n / 2 - 1; i >= 0; i--)
@@ -28,24 +26,30 @@ public class HeapSort implements SortTester {
             heapify(array, i, 0);
         }
     }
-        void heapify(int array[], int n, int i)
-        {
-            int largest = i;
-            int l = 2*i + 1;
-            int r = 2*i + 2;
 
-            if (l < n && array[l] > array[largest])
-                largest = l;
+    public static void heapify(int array[], int n, int i) {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+        if (l < n && array[l] > array[largest])
+            largest = l;
 
-            if (r < n && array[r] > array[largest])
-                largest = r;
+        if (r < n && array[r] > array[largest])
+            largest = r;
 
-            if (largest != i)
-            {
-                int swap = array[i];
-                array[i] = array[largest];
-                array[largest] = swap;
+        if (largest != i) {
+            int swap = array[i];
+            array[i] = array[largest];
+            array[largest] = swap;
 
-                heapify(array, n, largest);
-            }
+            heapify(array, n, largest);
         }
+    }
+
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+}
