357c8d105371c4bedefb11a08587541ffa552d3c
Victor Reyes <victor.reyes1@ujcv.edu.hn>
2020-03-20 00:14:23 -0600

    QuickSort en el Main

--- src/edu/ujcv/progra1/Main.java
+++ src/edu/ujcv/progra1/Main.java
@@ -12,5 +12,14 @@ public class Main {
         for (int i = 0; i < arr.length; i++){
             arr[i] = Math.abs(r.nextInt(10000));
         }
+
+        QuickSort qs = new QuickSort();
+        int n = arr.length;
+        qs.quickSort(arr, 0, n-1);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        System.out.println("\n\ntarda "+ s.sort(elementos)+ " milisegundos");
     }
 }
